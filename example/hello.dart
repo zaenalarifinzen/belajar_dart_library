a290eecb4000a0482f1f1e627652ccd861c62c68
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2025-01-30 14:35:20 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(){
+  print(sayHello("Zaenal"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
